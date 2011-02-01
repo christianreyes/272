045f8bb727f170b9fef915a40963a34f0b9195f8
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-01-31 22:46:56 -0500

    simplequotes added

---
+++ lab/4/quotes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleQuotes::Application.config.secret_token = '9e4b4298b9675fb209efed8bd4e684d333b15de3db56897ff098aa3c00d348df9680867fe6ee497e18a49c26df6ecd6e6d3375e8063a912240371cbb7bff1943'
---
+++ lab/4/quotes/db/migrate/20110201014007_create_quotes.rb
@@ -0,0 +1,14 @@
+class CreateQuotes < ActiveRecord::Migration
+  def self.up
+    create_table :quotes do |t|
+      t.string :body
+      t.string :source, :default => "Anonymous"
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :quotes
+  end
+end
