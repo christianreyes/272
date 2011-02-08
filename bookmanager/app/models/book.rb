dc6d009b1babd25ea57f3a19d8203c10b59d001c
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-02-07 20:25:26 -0500

    partials edited. working now

--- bookmanager/app/models/book.rb
+++ bookmanager/app/models/book.rb
@@ -2,8 +2,8 @@ class Book < ActiveRecord::Base
     attr_accessible :title, :year_published, :publisher_id, :author_ids
     
     belongs_to :publisher
-    has_many :book_authors
-    has_many :authors, :through => :book_authors
+    has_many :bookauthors
+    has_many :authors, :through => :bookauthors
     
     validates_presence_of :title
     validates_numericality_of :year_published
