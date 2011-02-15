0ada66d1c627014d32aa0dadf0999992e9b72424
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-02-14 20:53:16 -0500

    formtastic. changed layout

--- lab/6/ChoreTracker/db/migrate/20110215004659_add_default_user.rb
+++ lab/6/ChoreTracker/db/migrate/20110215004659_add_default_user.rb
@@ -1,7 +1,15 @@
 class AddDefaultUser < ActiveRecord::Migration
   def self.up
+    dad = User.new
+    dad.username = "dad"
+    dad.email = "dad@yipee.com"
+    dad.password = "taskmaster"
+    dad.password_confirmation = "taskmaster"
+    dad.save!
   end
 
   def self.down
+    dad = User.where("username = ?", "dad").first
+    User.delete dad
   end
 end
--- lab/6/ChoreTracker/db/schema.rb
+++ lab/6/ChoreTracker/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110215003013) do
+ActiveRecord::Schema.define(:version => 20110215004659) do
 
   create_table "children", :force => true do |t|
     t.string   "first_name"
