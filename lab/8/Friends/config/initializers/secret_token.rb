bf073353e829b132cdf40c398d4db19bc007cf4b
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-03-14 19:31:27 -0400

    initial commit friends

---
+++ lab/8/Friends/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friends::Application.config.secret_token = '5ad176a77c64bf544f44167542d52761f95cd983b3bfa40e4a0151c046a59ba5ec37cc707cdc7c54d0cae363b13d14b1d0d5a907f7c0faa18968ae12cd6161d3'
