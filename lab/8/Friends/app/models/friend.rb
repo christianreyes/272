6c56b0fe1d1977cdf2541386c56ffebdd3831988
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-03-14 19:50:41 -0400

    select added

--- lab/8/Friends/app/models/friend.rb
+++ lab/8/Friends/app/models/friend.rb
@@ -1,2 +1,12 @@
 class Friend < ActiveRecord::Base
+	
+	validates_presence_of :name, :email, :level
+	validates_format_of :email, :with =>  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+	
+	FRIENDSHIP_LEVELS = [ ["Casual", 1], 
+	 					  ["Good", 2], 
+	 					  ["Close", 3], 
+	 					  ["Best", 4] ]
+	 					  
+	 
 end
